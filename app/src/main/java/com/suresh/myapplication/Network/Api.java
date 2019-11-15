c0136138dc4f1d55fd3f5c5145d45f2ee169e506
sureshkumarch <sureshc5551@gmail.com>
2019-11-15 14:49:32 +0530

    Retrofit Cache Service

--- app/src/main/java/com/suresh/myapplication/Network/Api.java
+++ app/src/main/java/com/suresh/myapplication/Network/Api.java
@@ -7,11 +7,15 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+import static com.suresh.myapplication.Network.HttpService.HEADER_CACHE;
 
 public interface Api {
 
     String BASE_URL = "https://dl.dropboxusercontent.com";
 
     @GET("/s/2iodh4vg0eortkl/facts.json")
+    @Headers(HEADER_CACHE + ": 60")
     Call<DataModel> getData();
 }
