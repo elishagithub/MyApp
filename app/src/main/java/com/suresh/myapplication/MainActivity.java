b63185fd561d2bc2f44cbedebfc5d048808bdfa1
sureshkumarch <sureshc5551@gmail.com>
2019-11-15 14:03:49 +0530

    Setting action bar title from web service api title.

--- app/src/main/java/com/suresh/myapplication/MainActivity.java
+++ app/src/main/java/com/suresh/myapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package com.suresh.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+
+import com.suresh.myapplication.Adapters.CustomAdapter;
+import com.suresh.myapplication.Models.DataModel;
+import com.suresh.myapplication.ViewModels.DataViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DataViewModel model = ViewModelProviders.of(this).get(DataViewModel.class);
+
+        model.getData().observe(this, new Observer<DataModel>() {
+            @Override
+            public void onChanged(@Nullable DataModel list) {
+
+                getSupportActionBar().setTitle(list.getTitle());
+
+            }
+        });
+
+
     }
 }
